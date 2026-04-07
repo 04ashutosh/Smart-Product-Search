52265c56776f736ec1b0cfb687ebc0b929bbdb41
Ashutosh <ashutosh04004@gmail.com>
2026-04-07 23:28:28 +0530

    Add ElasticsearchConfig

---
+++ backend/src/main/java/com/search/api/config/ElasticsearchConfig.java
@@ -0,0 +1,19 @@
+package com.search.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+    @Value("${spring.elasticsearch.uris:localhost:9200}")
+    private String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration(){
+        return ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl.replace("http://","").replace("https://",""))
+                .build();
+    }
+}
